4bd9aa8aff5e8a2ded6150d2f00d5ed63e3d301c
Sven Jansen <sven.jansen@devicetrust.com>
2022-12-23 14:30:23 +0100

    .

--- main.tf
+++ main.tf
@@ -30,32 +30,32 @@ data "vsphere_network" "network" {
 data "vsphere_ovf_vm_template" "ovfLocal" {
   name              = "TemporaryName"
   disk_provisioning = "thin"
-  resource_pool_id  = data.vsphere.resource_pool.pool.id
-  datastore_id      = data.vsphere.datastore.datastore.id
-  host_system_id    = data.vsphere.host.host.id
+  resource_pool_id  = data.vsphere_resource_pool.pool.id
+  datastore_id      = data.vsphere_datastore.datastore.id
+  host_system_id    = data.vsphere_host.host.id
   local_ovf_path    = var.vm.ovf
 
   ovf_network_map = {
-    "VM Network" : data.vsphere.network.network.id
+    "VM Network" : data.vsphere_network.network.id
   }
 }
 
 resource "vsphere_virtual_machine" "build_citrix-adc" {
   name                 = var.vm.name
   datacenter_id        = data.vsphere_datacenter.dc.id
-  resource_pool_id     = data.vsphere.resource_pool.pool.id
-  datastore_id         = data.vsphere.datastore.datastore.id
-  host_system_id       = data.vsphere.host.host.id
+  resource_pool_id     = data.vsphere_resource_pool.pool.id
+  datastore_id         = data.vsphere_datastore.datastore.id
+  host_system_id       = data.vsphere_host.host.id
   # folder               = var.vm.folder
-  num_cpus             = data.vsphere.ovf_vm_template.ovfLocal.num_cpus
-  num_cores_per_socket = data.vsphere.ovf_vm_template.ovfLocal.num_cores_per_socket
-  memory               = data.vsphere.ovf_vm_template.ovfLocal.memory
-  guest_id             = data.vsphere.ovf_vm_template.ovfLocal.guest_id
-  scsi_type            = data.vsphere.ovf_vm_template.ovfLocal.scsi_type
-  nested_hv_enabled    = data.vsphere.ovf_vm_template.ovfLocal.nested_hv_enabled
+  num_cpus             = data.vsphere_ovf_vm_template.ovfLocal.num_cpus
+  num_cores_per_socket = data.vsphere_ovf_vm_template.ovfLocal.num_cores_per_socket
+  memory               = data.vsphere_ovf_vm_template.ovfLocal.memory
+  guest_id             = data.vsphere_ovf_vm_template.ovfLocal.guest_id
+  scsi_type            = data.vsphere_ovf_vm_template.ovfLocal.scsi_type
+  nested_hv_enabled    = data.vsphere_ovf_vm_template.ovfLocal.nested_hv_enabled
   
   network_interface {
-    network_id     = values(data.vsphere.ovf_vm_template.ovfLocal.ovf_network_map)[0]
+    network_id     = values(data.vsphere_ovf_vm_template.ovfLocal.ovf_network_map)[0]
     use_static_mac = true
     mac_address    = var.vm.mac
   }
@@ -65,9 +65,9 @@ resource "vsphere_virtual_machine" "build_citrix-adc" {
 
   ovf_deploy {
     allow_unverified_ssl_cert = false
-    local_ovf_path            = data.vsphere.ovf_vm_template.ovfLocal.local_ovf_path
-    disk_provisioning         = data.vsphere.ovf_vm_template.ovfLocal.disk_provisioning
-    ovf_network_map           = data.vsphere.ovf_vm_template.ovfLocal.ovf_network_map
+    local_ovf_path            = data.vsphere_ovf_vm_template.ovfLocal.local_ovf_path
+    disk_provisioning         = data.vsphere_ovf_vm_template.ovfLocal.disk_provisioning
+    ovf_network_map           = data.vsphere_ovf_vm_template.ovfLocal.ovf_network_map
   }
 
   extra_config = {
